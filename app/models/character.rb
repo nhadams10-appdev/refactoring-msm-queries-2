c0591dddd8b48bbf56086af97e89ad8679450f54
Noah Adams <nhadams10@uchicago.edu>
2021-02-28 04:34:27 +0000

    Finished Refactoring! Very exciting tools

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,13 +10,36 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    my_movie_id = self.movie_id
+  # def movie
+  #   my_movie_id = self.movie_id
 
-    matching_movies = Movie.where({ :id => my_movie_id })
+  #   matching_movies = Movie.where({ :id => my_movie_id })
 
-    the_movie = matching_movies.at(0)
+  #   the_movie = matching_movies.at(0)
 
-    return the_movie
-  end
+  #   return the_movie
+  # end
+
+  belongs_to(:movie, {
+      :class_name => "Movie",
+      :foreign_key => "movie_id",
+      :required => false
+  })
+
+  # def actor
+  #   the_foreign_key = self.actor_id
+
+  #   matching_records = Actor.where({ :id => the_foreign_key })
+
+  #   the_record = matching_records.at(0)
+
+  #   return the_record
+  # end
+
+  belongs_to(:actor, {
+    :class_name => "Actor",
+    :foreign_key => "actor_id",
+    :required => true
+  })
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,14 +12,31 @@
 #  updated_at  :datetime         not null
 #  director_id :integer
 #
+
 class Movie < ApplicationRecord
-  def director
-    my_director_id = self.director_id
+#  def director
+#    my_director_id = self.director_id
+#
+#    matching_directors = Director.where({ :id => my_director_id })
+#
+#    the_director = matching_directors.at(0)
+#
+#    return the_director
+#  end
 
-    matching_directors = Director.where({ :id => my_director_id })
+  belongs_to(:director, { :foreign_key => "director_id", :class_name => "Director" })
+  #same as
+  #belongs_to(:director)
 
-    the_director = matching_directors.at(0)
+  # def characters
+  #   my_id = self.id
+  
+  #   matching_records = Character.where({ :movie_id => my_id })
 
-    return the_director
-  end
+  #   return matching_records
+  # end
+
+  has_many(:characters, { :class_name => "Character", :foreign_key => "movide_id" })
+  #same as
+  #has_many(:characters)
 end
